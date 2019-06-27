dabac0150aefaadcdb57caf0495d462c69cd496d
Warren-Lamb <10warren10@gmail.com>
2019-06-27 04:05:55 -0400

    add main and regex-paths

--- src/main.rs
+++ src/main.rs
@@ -20,5 +20,40 @@ const INDEX: &str = r#"
 </html>
 "#;
 
+lazy_static! {
+	static ref INDEX_PATH : Regex = Regex::new("^/(index\\.html?)?$").unwrap();
+	static ref USER_PATH: Regex = Regex::new("^/user/((?P<user_id>\\d+?/?)?$").unwrap();
+	static ref USERS_PATH: Regex = Regex::new("^/users/?$").unwrap();
+}
+
+type UserId = u64;
+struct UserData;
+type UserDb = Arc<Mutex<Slab<user_data>>>;
+
+impl fmt::Display for UserData {
+	fn fmt(&self, f: &mut fmt::formatter) -> fmt::result {
+		f.write_str("{}")
+	}
+}
+
+fn main(){
+	let address = ([127,0,0,1],6000).into();
+	let builder = Server::bind(address);
+	let db = Arc::new(Mutex::new(Slab::new()));
+	let server = builder.serve(move ||{
+		let db = db.clone();
+		Service_fn(move |req|{
+			microservice_handler(req: Request, &db: UserDb)
+		});
+	let server = server.map_err(drop);
+	hyper::rt::run(server);
+
+
+
+
+
+
+	})
+} 
 
 
